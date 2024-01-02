af0a67580c2c22eea14dc88486516551fe692cad
Garcayf <150861245+Garcayf@users.noreply.github.com>
2024-01-02 10:46:36 -0400

    some changes
    some changes

--- R/guille.R
+++ R/guille.R
@@ -6,29 +6,30 @@ guille_ui <- function(id, db) {
       sidebarPanel(
         selectizeInput(NS(id, "internal_balance"),
                        "Internal Balance",
-                       choices=NULL
+                       choices = NULL
         ),
         selectizeInput(NS(id, "external_balance"),
                        "External Balance",
-                       choices=NULL
+                       choices = NULL
         ),
         selectInput(NS(id, "year"), "Year",
-                    choices=unique(db$year),
-                    selected=2020
+                    choices = unique(db$year),
+                    selected = 2020
         ),
         sliderInput(NS(id, "year_range"), "Year Range",
-                    min=min(db$year),
-                    max=max(db$year),
-                    sep="",
+                    min = min(db$year),
+                    max = max(db$year),
+                    sep = "",
                     step = 1,
-                    value=c(1990, 2021)
+                    value = c(1990, 2021)
         ),
-        selectInput(NS(id, "test"), "Test Input", choices=c("A", "B", "C"), selected="A"),
+        selectInput(NS(id, "test"), "Test Input", choices = c("A", "B", "C"), selected = "A"),
         actionButton(NS(id, "explore_update"), "Update")
       ),
       mainPanel(
         tags$h3(""),
-        tags$h3("Test"),
+        tags$h3("Test Stacked Area Plot"),
+        plotlyOutput(NS(id, "current_account_plot")),
         tags$h2(""),
         tags$h3("BBNN"),
         plotlyOutput(NS(id, "bbnn_plot")),
@@ -50,28 +51,36 @@ guille_server <- function(id, country, comparators, db, labels, reverse_labels) 
 
   moduleServer(id, function(input, output, session) {
 
-    updateSelectizeInput(session, 'internal_balance', choices=labels, server=TRUE, selected="wdi_fp_cpi_totl_zg")
-    updateSelectizeInput(session, 'external_balance', choices=labels, server=TRUE, selected="wdi_bn_cab_xoka_gd_zs")
+    updateSelectizeInput(session, 'internal_balance', choices = labels, server = TRUE, selected = "wdi_fp_cpi_totl_zg")
+    updateSelectizeInput(session, 'external_balance', choices = labels, server = TRUE, selected = "wdi_bn_cab_xoka_gd_zs")
 
     output$bbnn_plot <- renderPlotly({
       # show the bbnn plot based on indicators selected
       # for the year chosen
-      bbnn_plot(input$internal_balance, input$external_balance, ccode=country(), df=db, fyear=input$year, reverse_labels=reverse_labels) %>%
+      bbnn_plot(input$internal_balance, input$external_balance, ccode=country(), df = db, fyear = input$year, reverse_labels = reverse_labels) %>%
         ggplotly()
     })
 
     output$internal_balance_plot <- renderPlot({
       db %>%
         filter(weo_countrycodeiso == country()) %>%
-        ggplot(aes_string(x="year", y=input$internal_balance)) +
+        ggplot(aes_string(x = "year", y = input$internal_balance)) +
         geom_line() +
         geom_point()
+    })
+
+    output$current_account_plot <- renderPlot({
+      db %>%
+        filter(weo_countrycodeiso == country()) %>%
+        gather(variable, value, wdi_bx_gsr_mrch_cd, wdi_bx_gsr_nfsv_cd, wdi_bx_gsr_fcty_cd, wdi_bx_trf_curr_cd, wdi_bm_gsr_mrch_cd, wdi_bm_gsr_nfsv_cd, wdi_bm_gsr_fcty_cd, wdi_bm_trf_prvt_cd) %>% # nolint: line_length_linter.
+        ggplot(aes(x = "year", y = value, fill = variable)) +
+        geom_area()
     })
 
     output$external_balance_plot <- renderPlot({
       db %>%
         filter(weo_countrycodeiso == country()) %>%
-        ggplot(aes_string(x="year", y=input$external_balance)) +
+        ggplot(aes_string(x = "year", y = input$external_balance)) +
         geom_line() +
         geom_point()
     })
--- shiny-run.R
+++ shiny-run.R
@@ -3,3 +3,4 @@ options(shiny.autoreload = TRUE)
 # pkgload::load_all(".")
 # macroTool()
 shiny::runApp(port=7777, launch.browser=TRUE)
+
